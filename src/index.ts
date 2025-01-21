95741fe8fb72adc787a70325ea85854347808f0f
dragon <ExplodingFKL@gmail.com>
2025-01-21 17:32:43 +0800

    初始化项目

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import { Context, Schema } from 'koishi'
 
-export const name = 'singleboy-help'
+export const name = 'single-boy-help'
+export const inject = ['database']
 
-export interface Config {}
+export interface Config {
+  single: Array<string>
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  single: Schema.array(String).description('自我帮助触发词').required(false).default(['撸','🦌']),
+})
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export function apply(ctx: Context,config: Config) {
+  ctx.on('message', msg => {
+    if (config.single.indexOf(msg.content)){
+      console.log(msg)
+    }
+  })
 }
