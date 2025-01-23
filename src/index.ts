cdf7747ba1241314f95ea86a96a0e587e3c9489a
dragon <ExplodingFKL@gmail.com>
2025-01-23 17:11:28 +0800

    初步完成项目

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,32 @@ import {Context, h, Schema} from 'koishi'
 import {SingleBoyHelper} from "./helper";
 import {parseCmdAt} from "./utils";
 
-export const name = 'single-boy-help'
+export const name = 'single-boy-helper'
 export const inject = ['database']
+export const usage = `
+## 一款简单易用的活跃群友气氛的软件
+`;
 
 
 export interface Config {
   // 自我帮助关键词
   help: Array<string>,
   // 帮助他人关键词
-  forceHelp: Array<string>,
+  force: Array<string>,
   // 绑定好友关键词
   bind: Array<string>,
   // 解绑好友关键词
   unbind: Array<string>,
-  // Ollama 地址
+  // 榜单
+  rank: string,
 }
 
 export const Config: Schema<Config> = Schema.object({
   help: Schema.array(String).description('帮助他人触发词').default(['🦌','帮🦌']),
-  forceHelp: Schema.array(String).description('强行帮助他人触发词').default(['强🦌']),
+  force: Schema.array(String).description('强行帮助他人触发词').default(['强🦌']),
   bind: Schema.array(String).description('绑定好友关键词').default(['添加🦌友']),
   unbind: Schema.array(String).description('解绑好友关键词').default(['解除🦌友关系']),
+  rank: Schema.string().description('查询榜单').default('今日🦌榜'),
 })
 
 export function apply(ctx: Context, config: Config) {
@@ -37,7 +42,7 @@ export function apply(ctx: Context, config: Config) {
           return helper.Other(session, result.data)
         }
       }
-      result = parseCmdAt(session, config.forceHelp);
+      result = parseCmdAt(session, config.force);
       if (result.ok) {
         if (result.data.length == 0) {
           return helper.Self(session)
@@ -53,7 +58,7 @@ export function apply(ctx: Context, config: Config) {
       if (result.ok) {
         return helper.Unbind(session, result.data);
       }
-      if (session.content === '今日🦌榜') {
+      if (session.content === config.rank) {
         return await helper.Rankings(session)
       }
       return Promise.any('')
